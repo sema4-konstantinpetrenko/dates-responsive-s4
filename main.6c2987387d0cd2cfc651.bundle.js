cb6640af6d95a896efbc0500c1a0ec4528d77bf7
Konstantin <kpetrenko@varteq.com>
2020-04-17 16:06:13 +0300

    publish: Package rename
    generated from commit 92ad0b22eaa1392da2d9bc3a072c8c743fbb0a2d

---
+++ main.6c2987387d0cd2cfc651.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1235:function(n,o,c){"use strict";c.r(o);c(1236),c(1267)},418:function(n,o,c){c(419),c(562),n.exports=c(1235)},481:function(n,o){}},[[418,1,2]]]);
